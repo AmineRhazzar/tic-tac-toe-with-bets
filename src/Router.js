bd1e43463efa018e474736507b88eb811829b00c
Mohammed El Amine Rhazzar <mohammedelaminerhazzar@gmail.com>
2021-03-25 00:24:20 +0100

    fixed a bug where player name wasn't always updated synchronously

--- src/Router.js
+++ src/Router.js
@@ -12,7 +12,6 @@ class Home extends React.Component {
     }
 
     createRoomCode = (chosenUsername) => {
-        this.setState({ newRoomCode: 'azert' });
         const newRoomState = {
             "turn": 2,
             "board": [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -21,7 +20,7 @@ class Home extends React.Component {
             "isFormVisible": false
         };
         updateRoomState(newRoomState);
-        
+        this.setState({ newRoomCode: 'azert' });
     }
 
     joinRoom = (chosenUsername) => {
